a2937fccf76485017422fc445c7d9cdb3647e561
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-11-29 16:10:36 +0100

    ex11

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+const app = express();
+
+const { SERVER_PORT } = process.env;
+
+app.use(morgan("dev"));
+
+type Planet = {
+  id: number,
+  name: string,
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
+
+app.get('/planets', (_, res) => {
+  res.status(200).json(planets);
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server up and running in port ${SERVER_PORT}`);
+});
